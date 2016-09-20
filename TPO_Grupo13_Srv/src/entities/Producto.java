8aed963b1da3f4390f2991f85bed229744451895
sebastiansz84 <sebastiansz@gmail.com>
2016-09-20 20:25:43 -0300

    Saqué includes en desuso.

--- TPO_Grupo13_Srv/src/entities/Producto.java
+++ TPO_Grupo13_Srv/src/entities/Producto.java
@@ -2,9 +2,14 @@ package entities;
 
 import java.io.Serializable;
 import java.sql.Date;
-import java.util.Calendar;
 
-import javax.persistence.*;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
 
 @Entity
 @Table(name="Producto")
