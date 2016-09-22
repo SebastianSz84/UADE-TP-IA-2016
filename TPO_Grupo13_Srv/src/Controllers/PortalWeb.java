395e260c7c1ebd78335c46eb586a40ffb05c0679
Leonardo Esmoris <leonardo.esmoris@axa-assistance.com>
2016-09-22 19:37:19 -0300

    Agrego ejemplo con ResultadoOperacion

--- TPO_Grupo13_Srv/src/Controllers/PortalWeb.java
+++ TPO_Grupo13_Srv/src/Controllers/PortalWeb.java
@@ -4,8 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 import Dao.ProductoDAO;
+import Dao.UsuarioDAO;
 import Dto.ProductoDTO;
+import Dto.ResultadoOperacion;
 import Entities.Producto;
+import Entities.Usuario;
 
 public class PortalWeb {
 	private static PortalWeb instancia;
@@ -27,5 +30,34 @@ public class PortalWeb {
 			lista.add(p.getDTO());
 		}
 		return lista;
+	}
+
+	public ResultadoOperacion altaUsuario(String nombre, String apellido, String userName, String password) {
+		if (nombre.trim().isEmpty())
+			return new ResultadoOperacion(false, "El nombre no puede estar vacio");
+
+		if (nombre.trim().isEmpty())
+			return new ResultadoOperacion(false, "El apellido no puede estar vacio");
+
+		if (nombre.trim().isEmpty())
+			return new ResultadoOperacion(false, "El username no puede estar vacio");
+
+		if (nombre.trim().isEmpty())
+			return new ResultadoOperacion(false, "El password no puede estar vacio");
+
+		try {
+
+			Usuario usuario = new Usuario();
+			usuario.setNombre(nombre);
+			usuario.setApellido(apellido);
+			usuario.setUserName(userName);
+			usuario.setPassword(password);
+
+			UsuarioDAO.saveEntity(usuario);
+
+			return new ResultadoOperacion(true, "Usuario creado con exito");
+		} catch (Exception ex) {
+			return new ResultadoOperacion(false, "Error al crear usuario : " + ex.getMessage());
+		}
 	}
 }
