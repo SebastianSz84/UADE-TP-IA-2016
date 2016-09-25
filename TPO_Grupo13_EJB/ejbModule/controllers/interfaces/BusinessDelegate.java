09f044ccdd905a88fcd6101ce263247a372211d8
sebastiansz84 <sebastiansz@gmail.com>
2016-09-25 14:06:48 -0300

    Cosas varias (no me acuerdo qué hice).

---
+++ TPO_Grupo13_EJB/ejbModule/controllers/interfaces/BusinessDelegate.java
@@ -0,0 +1,23 @@
+package controllers.interfaces;
+
+import java.rmi.RemoteException;
+import java.util.List;
+
+import dto.CarritoDTO;
+import dto.ProductoDTO;
+import dto.UsuarioDTO;
+import dto.VentaDTO;
+
+public interface BusinessDelegate {
+	public List<ProductoDTO> listadoProductos() throws RemoteException;
+
+	public List<ProductoDTO> listadoBestSellers() throws RemoteException;
+
+	public void loginUsuario(String user, String pass) throws RemoteException;
+
+	public CarritoDTO crearCarrito(UsuarioDTO u) throws RemoteException;
+
+	public void modificarCarrito(CarritoDTO c) throws RemoteException;
+
+	public VentaDTO confirmarCarrito(CarritoDTO c) throws RemoteException;
+}
