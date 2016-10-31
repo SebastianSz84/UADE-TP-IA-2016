0e8d7d7c379fa4815145aced121f81f241b10aa8
sebastiansz84 <sebastiansz@gmail.com>
2016-10-31 19:27:04 -0300

    Arreglo en crearCarrito

---
+++ TPO_Grupo13_EJB/ejbModule/integracion/dto/NotificacionLMDTO.java
@@ -0,0 +1,32 @@
+package integracion.dto;
+
+public class NotificacionLMDTO {
+	private String fecha;
+	private String modulo;
+	private String descripcion;
+
+	public String getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(String fecha) {
+		this.fecha = fecha;
+	}
+
+	public String getModulo() {
+		return modulo;
+	}
+
+	public void setModulo(String modulo) {
+		this.modulo = modulo;
+	}
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+}
