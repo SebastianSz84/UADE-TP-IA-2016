496cb54092f0a7f609df1adf7c050760eca64a92
sebastiansz84 <sebastiansz@gmail.com>
2016-11-03 19:11:46 -0300

    Funciona el NuevoProductoMDB.

--- TPO_Grupo13_Web/src/servlets/TestNuevoProductoMDB.java
+++ TPO_Grupo13_Web/src/servlets/TestNuevoProductoMDB.java
@@ -2,7 +2,11 @@ package servlets;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Properties;
 
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.JMSContext;
 import javax.jms.Queue;
 import javax.jms.QueueConnection;
 import javax.jms.QueueConnectionFactory;
@@ -11,6 +15,7 @@ import javax.jms.QueueSession;
 import javax.jms.TextMessage;
 import javax.naming.Context;
 import javax.naming.InitialContext;
+import javax.naming.NamingException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -64,4 +69,64 @@ public class TestNuevoProductoMDB extends HttpServlet {
 
 	}
 
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		ProductoDTO prodDTO = new ProductoDTO();
+		prodDTO.setTipo("Tipo");
+		prodDTO.setCodigo(11);
+		prodDTO.setDatosExtra("algunos datos extra");
+		prodDTO.setDescripcion("alguna descripcion");
+		prodDTO.setMarca("alguna marca");
+		prodDTO.setNombre("algun nombre");
+		prodDTO.setOrigen("algun origen");
+		prodDTO.setPrecio(123.45678);
+		prodDTO.setFoto("alguna url de imagen");
+		prodDTO.setIdDeposito("G01");
+
+		String MESSAGE = ParserJson.toString(prodDTO);
+		String CONNECTION_FACTORY = "jms/RemoteConnectionFactory";
+		String DESTINATION = "jms/queue/deposito";
+
+		Context namingContext = null;
+		JMSContext context = null;
+
+		try {
+
+			// Set up the namingContext for the JNDI lookup
+			final Properties env = new Properties();
+			env.put(Context.INITIAL_CONTEXT_FACTORY, "org.jboss.naming.remote.client.InitialContextFactory");
+			env.put(Context.PROVIDER_URL, "http-remoting://10.100.34.205:8080");
+			env.put(Context.SECURITY_PRINCIPAL, "guest");
+			env.put(Context.SECURITY_CREDENTIALS, "guest");
+			namingContext = new InitialContext(env);
+
+			ConnectionFactory connectionFactory = (ConnectionFactory) namingContext.lookup(CONNECTION_FACTORY);
+			System.out.println("Se obtuvo ConnectionFactory " + CONNECTION_FACTORY);
+
+			Destination destination = (Destination) namingContext.lookup(DESTINATION);
+			System.out.println("Se obtuvo JMS Endpoint " + DESTINATION);
+
+			// ACT
+			// Create the JMS context
+			context = connectionFactory.createContext("guest", "guest");
+			context.createProducer().send(destination, MESSAGE);
+
+			// ASSERT
+			System.out.println("Mensaje enviado:  " + MESSAGE);
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if (namingContext != null) {
+				try {
+					namingContext.close();
+				} catch (NamingException e) {
+					e.printStackTrace();
+				}
+			}
+			if (context != null) {
+				context.close();
+			}
+		}
+	}
 }
